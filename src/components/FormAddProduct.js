b3351d7b32a31b7f8cdb6432693eb12345a4e420
resiwicaksono <resiwicaksono98@gmail.com>
2022-08-12 03:40:20 +0700

    auth mern frontend

---
+++ src/components/FormAddProduct.js
@@ -0,0 +1,62 @@
+import axios from 'axios'
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
+const FormAddProduct = () => {
+	const [name, setName] = useState("") 
+	const [price, setPrice] = useState("") 
+	const [message, setMessage] = useState("") 
+	const navigate = useNavigate()
+
+	const saveProduct = async (e) => {
+		e.preventDefault()
+		try {
+			await axios.post(`http://localhost:5000/products`, {
+				name: name,
+				price: price
+			})
+			navigate("/products")
+		} catch (error) {
+			if(error.response){
+				setMessage(error.response.data.msg)
+			}
+		}
+	}
+	return (
+		<div>
+			<h1 className='title'>Products</h1>
+			<h2 className='subtitle'>Add New Products</h2>
+			<div className="card is-shadowless">
+				<div className="card-content">
+					<div className="content">
+						<form onSubmit={saveProduct}>
+							<p className="has-text-centered">{message}</p>
+							<div className="field">
+								<label className="label">Name</label>
+								<div className="control">
+									<input type="text" className="input" value={name} onChange={(e) => setName(e.target.value)} placeholder='Product Name' />
+								</div>
+							</div>
+							<div className="field">
+								<label className="label">Price</label>
+								<div className="control">
+									<input type="text" className="input" value={price} onChange={(e) => setPrice(e.target.value)}  placeholder='Price' />
+								</div>
+							</div>
+
+							<div className="field">
+								<div className="control">
+									<button type='submit' className="button is-success">Save</button>
+								</div>
+
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default FormAddProduct
---
+++ src/components/Navbar.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import {LogOut,reset} from '../features/authSlice'
+
+
+function Navbar() {
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
+	const {user} = useSelector((state) => state.auth)
+
+	const logout = () => {
+		dispatch(LogOut())
+		dispatch(reset())
+		navigate("/")
+	}
+
+	return (
+		<div>
+			<nav className="navbar is-fixed-top has-shadow" role="navigation" aria-label="main navigation">
+				<div className="navbar-brand">
+					<NavLink className="navbar-item " to="/dashboard">
+						<h1 className='is-size-3 is-capitalized has-text-weight-bold'>Authentication</h1>
+					</NavLink>
+
+					<a role="button" href='!#' className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+						<span aria-hidden="true"></span>
+						<span aria-hidden="true"></span>
+						<span aria-hidden="true"></span>
+					</a>
+				</div>
+
+				<div id="navbarBasicExample" className="navbar-menu">
+
+					<div className="navbar-end">
+						<div className="navbar-item">
+							<div className="buttons">
+								<button onClick={logout} className="button is-light">
+									Log out
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</nav>
+		</div>
+	)
+}
+
+export default Navbar
